9802347e834df4a45f1b6c17ebf42cfa5e6cb2fd
tymaestro <tymaestro22@gmail.com>
2022-08-05 16:58:43 +0000

    create base template, create home app, wire up view with index page

--- biketours/settings.py
+++ biketours/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'shop',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -65,7 +66,10 @@ ROOT_URLCONF = 'biketours.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
