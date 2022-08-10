6c05873caaca6f1f88e1a8c8a69795127c238b38
tymaestro <tymaestro22@gmail.com>
2022-08-10 20:25:45 +0000

    create tours view in products app and wire up template in urls

--- products/views.py
+++ products/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Tour
 
-# Create your views here.
+
+def all_tours(request):
+
+    tours = Tour.objects.all()
+
+    context = {
+        'tours': tours,
+    }
+
+    return render(request, 'tours/tours.html', context)
