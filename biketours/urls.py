1518b97fb5d11a8ea06becbcd07e8bae612e1340
tymaestro <tymaestro22@gmail.com>
2022-08-12 18:09:36 +0000

    create basket template, connect urls and views, add bootstrap template to basket.html

--- biketours/urls.py
+++ biketours/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
     path('tours/', include('products.urls')),
+    path('basket/', include('shop.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def basket(request):
+    return render(request, 'basket/basket.html')
