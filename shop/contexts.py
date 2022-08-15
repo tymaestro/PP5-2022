5d45a330beaa1e51eb270a3dae4cdf8d281f1126
tymaestro <tymaestro22@gmail.com>
2022-08-15 16:02:48 +0000

    update basket_contents function in contexts.py

--- shop/contexts.py
+++ shop/contexts.py
@@ -7,6 +7,17 @@ def basket_contents(request):
     basket_items = []
     grand_total = 0
     tour_count = 0
+    basket = request.session.get('basket', {})
+
+    for item_id, quantity in basket.items():
+        tour = get_object_or_404(Tour, pk=item_id)
+        grand_total = quantity * tour.tour_price
+        tour_count += quantity
+        basket_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'tour': tour,
+        })
 
     context = {
         'basket_items': basket_items,
