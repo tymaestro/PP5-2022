709e09134912244bb7ae9214b7ffa0a1e5d46af8
tymaestro <tymaestro22@gmail.com>
2022-08-18 18:29:50 +0000

    add quantity field to Tour model, css styling to basket table elements

--- shop/contexts.py
+++ shop/contexts.py
@@ -9,26 +9,15 @@ def basket_contents(request):
     tour_count = 0
     basket = request.session.get('basket', {})
 
-    for tour_id, tour_data in basket.items():
-        if isinstance(tour_data, int):
-            tour = get_object_or_404(Tour, pk=tour_id)
-            total += tour_data * tour.tour_price
-            tour_count += tour_data
-            basket_items.append({
-                'tour_id': tour_id,
-                'quantity': tour_data,
-                'tour': tour,
-            })
-        else:
-            tour = get_object_or_404(Tour, pk=tour_id)
-            for quantity in tour_data.items():
-                total += quantity * tour.tour_price
-                tour_count += quantity
-                basket_items.append({
-                    'tour_id': tour_id,
-                    'quantity': quantity,
-                    'tour': tour,
-                })
+    for tour_id, quantity in basket.items():
+        tour = get_object_or_404(Tour, pk=tour_id)
+        total += quantity * tour.tour_price
+        tour_count += quantity
+        basket_items.append({
+            'tour_id': tour_id,
+            'quantity': quantity,
+            'tour': tour,
+        })
 
     context = {
         'basket_items': basket_items,
