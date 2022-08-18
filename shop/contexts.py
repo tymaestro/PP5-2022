68004958f1736b39c0abcb624027dda91c7b118d
tymaestro <tymaestro22@gmail.com>
2022-08-18 14:36:53 +0000

    add total price for basket page

--- shop/contexts.py
+++ shop/contexts.py
@@ -5,23 +5,34 @@ from products.models import Tour
 def basket_contents(request):
 
     basket_items = []
-    grand_total = 0
+    total = 0
     tour_count = 0
     basket = request.session.get('basket', {})
 
-    for item_id, quantity in basket.items():
-        tour = get_object_or_404(Tour, pk=item_id)
-        grand_total = quantity * tour.tour_price
-        tour_count += quantity
-        basket_items.append({
-            'item_id': item_id,
-            'quantity': quantity,
-            'tour': tour,
-        })
+    for tour_id, tour_data in basket.items():
+        if isinstance(tour_data, int):
+            tour = get_object_or_404(Tour, pk=tour_id)
+            total += tour_data * tour.tour_price
+            tour_count += tour_data
+            basket_items.append({
+                'tour_id': tour_id,
+                'quantity': tour_data,
+                'tour': tour,
+            })
+        else:
+            tour = get_object_or_404(Tour, pk=tour_id)
+            for quantity in tour_data.items():
+                total += quantity * tour.tour_price
+                tour_count += quantity
+                basket_items.append({
+                    'tour_id': tour_id,
+                    'quantity': quantity,
+                    'tour': tour,
+                })
 
     context = {
         'basket_items': basket_items,
-        'grand_total': grand_total,
+        'total': total,
         'tour_count': tour_count,
     }
 
