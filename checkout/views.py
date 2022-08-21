18144719c850dc395bf70e7c29fb5b6b12a63809
tymaestro <tymaestro22@gmail.com>
2022-08-21 11:18:59 +0000

    add views, urls and boilerplate template for checkout app

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 
-# Create your views here.
+from .forms import OrderForm
+
+
+def checkout(request):
+    basket = request.session.get('basket', {})
+
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, template, context)
