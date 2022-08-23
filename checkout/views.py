9cb0b52b9d6070f7512da1883f3196c5ffd715d3
tymaestro <tymaestro22@gmail.com>
2022-08-23 14:10:59 +0000

    add core stripe functionality for checkout page

--- checkout/views.py
+++ checkout/views.py
@@ -1,17 +1,33 @@
 from django.shortcuts import render, redirect, reverse
+from django.conf import settings
 
 from .forms import OrderForm
+from shop.contexts import basket_contents
+
+import stripe
 
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     basket = request.session.get('basket', {})
+
+    current_basket = basket_contents(request)
+    total = current_basket['total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
 
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51LSknvBDlVjl8tOm3Y1HV6BUP4885DIbqRz1OczVV6PrfmcAkgjswgl56dJPU8yYaRi9aYaiKVWf29kvH3ZzVfdC00Zed3ADwI',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
