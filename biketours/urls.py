ea053df3030ff8f03048c87a8d6e68adfeb76709
tymaestro <tymaestro22@gmail.com>
2022-08-24 19:21:16 +0000

    create initial view for profiles app, connect urls and create boilerplate profile.html template

--- biketours/urls.py
+++ biketours/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('tours/', include('products.urls')),
     path('basket/', include('shop.urls')),
     path('checkout/', include('checkout.urls')),
+    path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
