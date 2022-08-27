d787179ef077e3794b560259f1f997b4e70030c1
tymaestro <tymaestro22@gmail.com>
2022-08-27 12:41:54 +0000

    add pagination to tours.html

--- biketours/settings.py
+++ biketours/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
+from django.contrib.messages import constants as messages
 import dj_database_url
 if os.path.isfile('env.py'):
     import env
@@ -57,6 +58,14 @@ INSTALLED_APPS = [
 
     'crispy_forms',
 ]
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
