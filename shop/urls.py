77d07c07e7e1be4a2e0aa048d52d9a40eacfa99a
tymaestro <tymaestro22@gmail.com>
2022-08-28 06:09:15 +0000

    link delete functionality to basket.html, add javascript to basket.html

--- shop/urls.py
+++ shop/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.view_basket, name='view_basket'),
-    path('add/<item_id>/', views.add_to_basket, name='add_to_basket'),
-    path('update/<item_id>/', views.update_basket, name='update_basket'),
-    path('delete/<item_id>/', views.delete_from_basket, name='delete_from_basket'),
+    path('add/<tour_id>/', views.add_to_basket, name='add_to_basket'),
+    path('update/<tour_id>/', views.update_basket, name='update_basket'),
+    path('delete/<tour_id>/', views.delete_from_basket, name='delete_from_basket'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
 from django.contrib import messages
 
 from products.models import Tour
@@ -8,40 +8,40 @@ def view_basket(request):
     return render(request, 'basket/basket.html')
 
 
-def add_to_basket(request, item_id):
-    tour = get_object_or_404(Tour, pk=item_id)
+def add_to_basket(request, tour_id):
+    tour = get_object_or_404(Tour, pk=tour_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     basket = request.session.get('basket', {})
 
-    if item_id in list(basket.keys()):
-        basket[item_id] += quantity
+    if tour_id in list(basket.keys()):
+        basket[tour_id] += quantity
         messages.success(request,
                          (f'Updated {tour.tour_title} '
-                          f'quantity to {basket[item_id]}'))
+                          f'quantity to {basket[tour_id]}'))
     else:
-        basket[item_id] = quantity
+        basket[tour_id] = quantity
         messages.success(request, f'Added {tour.tour_title} to your basket')
 
     request.session['basket'] = basket
     return redirect(redirect_url)
 
 
-def update_basket(request, item_id):
+def update_basket(request, tour_id):
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     basket = request.session.get('basket', {})
 
-    basket[item_id] = quantity
+    basket[tour_id] = quantity
     request.session['basket'] = basket
 
     return redirect(redirect_url)
 
 
-def delete_from_basket(request, item_id):
+def delete_from_basket(request, tour_id):
     basket = request.session.get('basket', {})
 
-    basket.pop(item_id)
+    basket.pop(tour_id)
     request.session['basket'] = basket
 
     return redirect(reverse('view_basket'))
